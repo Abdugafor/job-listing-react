fc3237f97dbcf4d72155594e21b6849d9950be3e
Abdugafor Laptop <abdugaforgafarov0111@gmail.com>
2023-05-24 21:50:46 +0500

    new

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useState } from "react";
+
+const getData = async (url) => {
+  const res = await fetch(url)
+
+  return await res.json()
+}
 
 function App() {
+  const [jobs, setJobs] = useState({})
+  getData('http://localhost:3000/jobs').then(data => console.log(data))
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h2>Hello</h2>
   );
 }
 
